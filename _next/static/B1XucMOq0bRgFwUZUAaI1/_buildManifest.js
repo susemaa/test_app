ef0627a0c8c8bc0fed3c9e3afe7fdcea6318ee30
Arseny Radoslavov <rad.archeg@gmail.com>
2024-01-18 00:29:35 +0700

    Updates

---
+++ _next/static/B1XucMOq0bRgFwUZUAaI1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/733-d819840e4172b438.js","static/chunks/pages/index-c54f220a0e96fc17.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
