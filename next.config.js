ee400f3a34122c4894da23f44d5e3d0b1f0da569
Arseny Radoslavov <rad.archeg@gmail.com>
2024-01-18 00:46:20 +0700

    some fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/test_app',
+  assetPrefix: '/test_app/',
+  output: 'export',
 }
 
 module.exports = nextConfig
